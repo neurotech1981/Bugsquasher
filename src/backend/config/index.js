0bcc16ca5fd1b505717e57159502908f30e0a669
Neurotech81 <ba.jakobsen@gmail.com>
2020-07-06 15:23:40 +0200

    Sigh.

--- src/backend/config/index.js
+++ src/backend/config/index.js
@@ -1,6 +1,6 @@
 const config = {
   port: process.env.PORT || 3001,
-  jwtSecret: process.env.JWT_SECRET || "946Dypew!",
+  jwtSecret: process.env.JWT_SECRET || "<insert password>",
   mongoURI: process.env.MONGODB_URI || "mongodb://localhost/bugsquasher"
 };
 

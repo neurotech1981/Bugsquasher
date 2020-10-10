2cbe26267e84eeb64f98ce3180314a06590afd44
B A Jakobsen <ba.jakobsen@gmail.com>
2020-10-10 21:13:49 +0200

    updated password hash from SHA1 to SHA256

--- src/backend/src/models/user.js
+++ src/backend/src/models/user.js
@@ -53,7 +53,7 @@ userSchema.methods = {
     if (!password) return ''
     try {
       return crypto
-        .createHmac('sha1', this.salt)
+        .createHmac('sha256', this.salt)
         .update(password)
         .digest('hex')
     } catch (err) {
